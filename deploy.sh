0fd2a5818b6f3233bebb716c9b070b21287a607e
Master-Frank <1486493017@qq.com>
2021-11-06 13:30:14 +0800

    test github actions

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-#------------------------------------------
-
-#url访问目录，这个是你 github 仓库的名字
-#initDist "module.exports = '/Frank-Notes/'"
-
 # 生成静态文件
 npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github
+#-------deploy to github---------------------
+
+#url访问目录，这个是你 github 仓库的名字
+#initDist "module.exports = '/Frank-Notes/'"
+
 if [ -z "$GITHUB_TOKEN" ]; then
   # 手动部署
   msg='deploy'
@@ -30,21 +29,10 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
-
-cd - # 退回开始所在目录
-rm -rf docs/.vuepress/dist
-
-#------------------------------------------
-
-
-#打包代码同步到 gitee gh-pages分支
+#-----------同步到 gitee gh-pages分支------------------
 if [ -z "$SSH_PRIVATE_KEY" ]; then
   echo '如果是空字符串，则不部署到gitee'
 else
-  # 生成静态文件
-  npm run build
-  # 进入生成的文件夹
-  cd docs/.vuepress/dist
 
   giteeUrl=git@gitee.com:frank-starbuck/frank-notes.git  #gitee 仓库ssh地址
   
@@ -54,9 +42,10 @@ else
   git add -A
   git commit -m "来自github actions的自动部署"
   git push -f $giteeUrl master:gh-pages
-  
-  cd - # 退回开始所在目录
-  rm -rf docs/.vuepress/dist
+
+#------------------------------------------
+cd - # 退回开始所在目录
+rm -rf docs/.vuepress/dist
   # 删除秘钥
   rm -rf ~/.ssh
 fi
