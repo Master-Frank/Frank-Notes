3bcaab6b5b11867a9e907de956e6e14955e1cef6
Master-Frank <1486493017@qq.com>
2021-11-11 20:51:05 +0800

    更新文章

--- WebHook.sh
+++ WebHook.sh
@@ -59,23 +59,18 @@ fi
 # git项目路径
 gitPath="/www/wwwroot/$1"
 # git 网址
-gitHttp="https://github.com/Master-Frank/Frank-Notes.git"
+gitHttp="https://gitee.com/frank-starbuck/Frank-Notes.git"
 
 echo "Web站点路径：$gitPath"
 
 # 判断项目路径是否存在
 if [ -d "$gitPath" ]; then
         cd $gitPath
-        # 判断是否存在git目录
-        if [ ! -d ".git" ]; then
-                echo "在该目录下克隆 git"
-                git clone -b gh-pages $gitHttp gittemp
-                mv gittemp/.git .
-                rm -rf gittemp
+        rm -rf *
+        git clone -b gh-pages $gitHttp gittemp
+        mv gittemp/* .
+        rm -rf gittemp
         fi
-        # 拉取最新的项目文件
-        # git reset --hard origin/master
-        # git pull
         # 设置目录权限
         chown -R www:www $gitPath
         echo "End"
